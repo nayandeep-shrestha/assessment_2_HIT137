8e2a48a008121aeddcd505718bffb7b932145bad
Nayandeep Shrestha <shresnayan@gmail.com>
2026-01-08 19:26:09 +1100

    feat: lower case encryption

--- Q1/Q1.py
+++ Q1/Q1.py
@@ -1,6 +1,23 @@
 import os
 
 def encryption(shift1, shift2):
+    with open("raw_text.txt", "r", encoding="utf-8") as raw_file, \
+        open("encrypted_text.txt", "w", encoding="utf-8") as encrypted_file:
+
+        for char in raw_file.read():
+            if char.islower():
+                if 'a' <= char <= 'm':
+                    shift = shift1 * shift2
+                    new_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+                else:
+                    shift = shift1 + shift2
+                    new_char = chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+
+            else:
+                new_char = char
+
+            encrypted_file.write(new_char)
+
     print(f"Encryption successfull")
 def decryption(shift1, shift2):
     print(f"Decryption successfull")
