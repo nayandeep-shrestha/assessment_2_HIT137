c36e562c4176f658642a60c06ef5db251b86f739
Rajat Shrestha <rajatshrestha0208@gmail.com>
2026-01-14 13:41:49 +1100

    Created function draw polygon

--- Q3/Q3.py
+++ Q3/Q3.py
@@ -1,6 +1,7 @@
 import turtle
 ninja = turtle.Turtle()
 
+# Recursive function to draw a single modified edge
 def drawEdges (length, depth ):
     if depth == 0:
         ninja.forward(length)
@@ -14,11 +15,21 @@ def drawEdges (length, depth ):
         ninja.left(60)
         drawEdges(length, depth -1)
 
+def drawPolygon(sides, length, depth):
+    angle = 360 / sides
+    for _ in range(sides):
+        drawEdges(length, depth)
+        ninja.left(angle)
+
+#------ Main Program ----------
 def main():
-    
+
+# --------- User Input ------------
     sides=int(input("enter the number of sides:"))
     length= int(input("enter the side length"))
     depth= int(input("enter the recursion depth:"))
+
+    drawPolygon(sides, length, depth)
 
     turtle.done ()
 
